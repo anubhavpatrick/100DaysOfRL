706724c1e6d6c572d8bb39fa03f8c948e2fe9f11
Anubhav Patrick <anubhavpatrick@Anubhavs-MacBook-Air.local>
2019-11-09 15:21:18 +0530

    Adding RL Lab for leadingindia

--- Day_12_RL_Own_Environment_GUI.py
+++ Day_12_RL_Own_Environment_GUI.py
@@ -1,225 +1,394 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-#May require to install opencv if it is not available: conda install -c menpo opencv
-
-#REMEMBER: Open CV used BGR image encoding format
+import numpy as np
+import keras.backend.tensorflow_backend as backend
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+from collections import deque
+import time
+import random
+from tqdm import tqdm
+import os
+from PIL import Image
 import cv2
 
-import numpy as np 
-from PIL import Image
-import pickle #to save and load python objects
-import matplotlib.pyplot as plt
-from matplotlib import style 
-import time
-import os
+
+DISCOUNT = 0.99
+REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
+MODEL_NAME = '2x256'
+MIN_REWARD = -200  # For model save
+MEMORY_FRACTION = 0.20
+
+# Environment settings
+EPISODES = 20_000
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
+#  Stats settings
+AGGREGATE_STATS_EVERY = 50  # episodes
+SHOW_PREVIEW = False
 
 
-# In[4]:
-
-
-style.use('ggplot')
-
-
-# In[5]:
-
-
-#Setting the constants
-SIZE = 10 #Grid size of environment will be 10*10
-EPISODES = 25000
-MOVE_PENALTY = 1
-ENEMY_PENALTY = 300
-FOOD_REWARD = 25
-epsilon = 1 #epsilon greedy policy
-EPS_DECAY = 0.9998
-SHOW_EVERY = 3000
-start_q_table = None #can be prior q table saved using pickle library
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
-
-
-# In[6]:
-
-
-PLAYER_N=1
-FOOD_N=2
-ENEMY_N=3
-#open cv using BGR format for image representation
-d={1:(255,0,0),
-   2:(0,255,0),
-   3:(0,0,255)
-}
-
-
-# In[7]:
-
-
-#Creating class for player, food and enemy
 class Blob:
-    def __init__(self): #Python constructor
-        self.x = np.random.randint(0,SIZE)
-        self.y = np.random.randint(0,SIZE)
-        #To Do: There can be a issue in which enemy and/or player and/or food land on the same cell
-    
-    #for debugging purposes
+    def __init__(self, size):
+        self.size = size
+        self.x = np.random.randint(0, size)
+        self.y = np.random.randint(0, size)
+
     def __str__(self):
-        return f"{self.x}, {self.y}"
-    
-    #operator overloading
-    def __sub__(self,second):
-        return (self.x - second.x, self.y-second.y)
-        
-    #defining possible actions by Blob agent
-    def action(self,choice): #very simplified discrete action space consisting of four actions only
+        return f"Blob ({self.x}, {self.y})"
+
+    def __sub__(self, other):
+        return (self.x-other.x, self.y-other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def action(self, choice):
+        '''
+        Gives us 9 total movement options. (0,1,2,3,4,5,6,7,8)
+        '''
         if choice == 0:
-            self.move(x=1,y=1)
-        elif choice==1:
-            self.move(x=-1,y=-1)
-        elif choice ==2:
-            self.move(x=-1,y=1)
-        elif choice ==3:
-            self.move(x=1,y=-1)
-        elif choice == 4:#Move right
-            self.move(x=1,y=0)
-        elif choice ==5 :#Move left
-            self.move(x=-1,y=0)
-        elif choice ==6: #Move Up
-            self.move(x=0,y=1)
-        elif choice ==7: #Move Down
-            self.move(x=0,y=-1)
-        #To Do: Add more choices
-    
-    def move(self, x=False,y=False):
-        #The agent will move either randomly or based on value passed in x or y
-        #if not x:#x is local var and self.x is class var
-        #    self.x = np.random.randint(-1,2)  
-        #else:
-        self.x += x
-            
-        #if not y:
-        #    self.y = np.random.randint(-1,2)  
-        #else:
-        self.y += y
-        
-        #We have to also ensure that blob does not move outside the boundaries
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)
+
+        elif choice == 4:
+            self.move(x=1, y=0)
+        elif choice == 5:
+            self.move(x=-1, y=0)
+
+        elif choice == 6:
+            self.move(x=0, y=1)
+        elif choice == 7:
+            self.move(x=0, y=-1)
+
+        elif choice == 8:
+            self.move(x=0, y=0)
+
+    def move(self, x=False, y=False):
+
+        # If no value for x, move randomly
+        if not x:
+            self.x += np.random.randint(-1, 2)
+        else:
+            self.x += x
+
+        # If no value for y, move randomly
+        if not y:
+            self.y += np.random.randint(-1, 2)
+        else:
+            self.y += y
+
+        # If we are out of bounds, fix!
         if self.x < 0:
             self.x = 0
-        elif self.x > (SIZE-1):
-            self.x = SIZE-1
-        
+        elif self.x > self.size-1:
+            self.x = self.size-1
         if self.y < 0:
             self.y = 0
-        elif self.y > (SIZE-1):
-            self.y = SIZE-1
+        elif self.y > self.size-1:
+            self.y = self.size-1
 
 
-# In[8]:
+class BlobEnv:
+    SIZE = 10
+    RETURN_IMAGES = True
+    MOVE_PENALTY = 1
+    ENEMY_PENALTY = 300
+    FOOD_REWARD = 25
+    OBSERVATION_SPACE_VALUES = (SIZE, SIZE, 3)  # 4
+    ACTION_SPACE_SIZE = 9
+    PLAYER_N = 1  # player key in dict
+    FOOD_N = 2  # food key in dict
+    ENEMY_N = 3  # enemy key in dict
+    # the dict! (colors)
+    d = {1: (255, 175, 0),
+         2: (0, 255, 0),
+         3: (0, 0, 255)}
+
+    def reset(self):
+        self.player = Blob(self.SIZE)
+        self.food = Blob(self.SIZE)
+        while self.food == self.player:
+            self.food = Blob(self.SIZE)
+        self.enemy = Blob(self.SIZE)
+        while self.enemy == self.player or self.enemy == self.food:
+            self.enemy = Blob(self.SIZE)
+
+        self.episode_step = 0
+
+        if self.RETURN_IMAGES:
+            observation = np.array(self.get_image())
+        else:
+            observation = (self.player-self.food) + (self.player-self.enemy)
+        return observation
+
+    def step(self, action):
+        self.episode_step += 1
+        self.player.action(action)
+
+        #### MAYBE ###
+        #enemy.move()
+        #food.move()
+        ##############
+
+        if self.RETURN_IMAGES:
+            new_observation = np.array(self.get_image())
+        else:
+            new_observation = (self.player-self.food) + (self.player-self.enemy)
+
+        if self.player == self.enemy:
+            reward = -self.ENEMY_PENALTY
+        elif self.player == self.food:
+            reward = self.FOOD_REWARD
+        else:
+            reward = -self.MOVE_PENALTY
+
+        done = False
+        if reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY or self.episode_step >= 200:
+            done = True
+
+        return new_observation, reward, done
+
+    def render(self):
+        img = self.get_image()
+        img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
+        cv2.imshow("image", np.array(img))  # show it!
+        cv2.waitKey(1)
+
+    # FOR CNN #
+    def get_image(self):
+        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
+        env[self.food.x][self.food.y] = self.d[self.FOOD_N]  # sets the food location tile to green color
+        env[self.enemy.x][self.enemy.y] = self.d[self.ENEMY_N]  # sets the enemy location to red
+        env[self.player.x][self.player.y] = self.d[self.PLAYER_N]  # sets the player tile to blue
+        img = Image.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
+        return img
 
 
-#States of our Q Table consist of difference between x and y coordinates of the player and food Blob AND player and enemy Blob
-if start_q_table is None:
-    q_table = {} #dictionary
-    for x1 in range(-SIZE+1, SIZE):
-        for y1 in range(-SIZE+1, SIZE):
-            for x2 in range(-SIZE+1, SIZE):
-                for y2 in range(-SIZE+1,SIZE):
-                    q_table[((x1,y1),(x2,y2))]= [np.random.uniform(-5,0) for i in range(8)] #since there are eight discrete actions
-                    #The initial values need to be modified to see the impact
-                    
-else: #The Q Table exists and may be present in the form of pickle object
-    with open(start_q_table,"rb") as f:
-        q_table = pickle.load(f)
+env = BlobEnv()
+
+# For stats
+ep_rewards = [-200]
+
+# For more repetitive results
+random.seed(1)
+np.random.seed(1)
+tf.set_random_seed(1)
+
+# Memory fraction, used mostly when trai8ning multiple agents
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
+#backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
+
+# Create models folder
+if not os.path.isdir('models'):
+    os.makedirs('models')
 
 
-# ### Q Learning Algorithm
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
 
-# In[11]:
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
 
 
-START_EPSILON_DECAYING = 1#From which episode we want to start to decay epsilon
-END_EPSILON_DECAYING = EPISODES // 2 #Till which episode we want to decay epsilon
-epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
-episode_rewards = []
-for episode in range(EPISODES):
-    player = Blob()
-    food = Blob()
-    enemy = Blob()
-    
-    if episode % SHOW_EVERY == 0:
-        print(f'on #{episode}, epsilon: {epsilon}')
-        print(f'{SHOW_EVERY} ep mean {np.mean(episode_rewards[-SHOW_EVERY: ])}')
-        show = True
-    else:
-        show = False
-    
-    #updating epsilon value
-    
+# Agent class
+class DQNAgent:
+    def __init__(self):
+
+        # Main model
+        self.model = self.create_model()
+
+        # Target network
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        # An array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        # Custom tensorboard object
+        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
+
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+
+        model.add(Conv2D(256, (3, 3), input_shape=env.OBSERVATION_SPACE_VALUES))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Dense(64))
+
+        model.add(Dense(env.ACTION_SPACE_SIZE, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
+        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        return model
+
+    # Adds step's data to a memory replay array
+    # (observation space, action, reward, new observation space, done)
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    # Trains main network every step during episode
+    def train(self, terminal_state, step):
+
+        # Start training only if certain number of samples is already saved
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # Get current states from minibatch, then query NN model for Q values
+        current_states = np.array([transition[0] for transition in minibatch])/255
+        current_qs_list = self.model.predict(current_states)
+
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
+        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []
+        y = []
+
+        # Now we need to enumerate our batches
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            # Update Q value for given state
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
+
+        # Fit on all samples as one batch, log only on terminal state
+        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+
+        # Update target network counter every episode
+        if terminal_state:
+            self.target_update_counter += 1
+
+        # If counter reaches set value, update target network with weights of main network
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    # Queries main network for Q values given current observation space (environment state)
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+
+agent = DQNAgent()
+
+# Iterate over episodes
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+
+    # Update tensorboard step every episode
+    agent.tensorboard.step = episode
+
+    # Restarting episode - reset episode reward and step number
     episode_reward = 0
-    for i in range(200): #Here 200 is the steps in each episode. It is a hyperparameter
-        obs = (player - food, player - enemy)#Remember the function overriding
+    step = 1
+
+    # Reset environment and get initial state
+    current_state = env.reset()
+
+    # Reset flag and start iterating until episode ends
+    done = False
+    while not done:
+
+        # This part stays mostly the same, the change is to query a model for Q values
         if np.random.random() > epsilon:
-            action = np.argmax(q_table[obs])
+            # Get action from Q table
+            action = np.argmax(agent.get_qs(current_state))
         else:
-            action = np.random.randint(0,8)
-        
-        player.action(action)
-       
-        #Deciding the reward or penalty of agent after every step in each episode
-        if player.x == enemy.x and player.y == enemy.y:
-            reward = -ENEMY_PENALTY
-        elif player.x == food.x and player.y == food.y:
-            reward = FOOD_REWARD
-        else:
-            reward = -MOVE_PENALTY
-            
-        new_obs = (player-food,player-enemy)
-        max_future_q = np.max(q_table[new_obs])
-        current_q = q_table[obs][action]
-    
-        if reward == FOOD_REWARD:
-            new_q = FOOD_REWARD
-            q_table[obs][action] = new_q
-            episode_rewards.append(reward)
-            break
-        elif reward == -ENEMY_PENALTY:
-            new_q = -ENEMY_PENALTY
-            q_table[obs][action] = new_q
-            episode_rewards.append(reward)
-            break
-        else:
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
-            q_table[obs][action] = new_q
-            episode_rewards.append(reward)
-        
-        #Code to display the environment
-        if show:
-            env=np.zeros((SIZE,SIZE,3),dtype=np.uint8)
-            env[food.y][food.x] = d[FOOD_N]
-            env[player.y][player.x] = d[PLAYER_N]
-            env[enemy.y][enemy.x] = d[ENEMY_N]            
-            
-            img = Image.fromarray(env,'RGB')#from PIL library
-            img = img.resize((400,400))
-            cv2.imshow('',np.array(img))
-        
-    
-    # Decaying is being done every episode if episode number is within decaying range
-    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
-        epsilon -= epsilon_decay_value
+            # Get random action
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
 
+        new_state, reward, done = env.step(action)
 
-# In[ ]:
+        # Transform new continous state to new discrete state and count reward
+        episode_reward += reward
 
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
 
-x=0
-not x
+        # Every step we update replay memory and train main network
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done, step)
 
+        current_state = new_state
+        step += 1
 
-# In[ ]:
+    # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
-
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
 
